5dca3056c4eb448ef44ea5d1741d0dc879cc0110
Pavel Lazureykis <lazureykis@gmail.com>
2012-01-25 13:00:18 +0400

    publisher works with test config

--- lib/rabbit_jobs/configuration.rb
+++ lib/rabbit_jobs/configuration.rb
@@ -6,12 +6,38 @@ module RabbitJobs
       "localhost"
     end
 
-    def self.queue
+    def self.queue_params
       {
-        name: "rabbit_jobs_test",
-        params: {
-          # auto_delete: true
-        }
+        auto_delete: false,
+        ack: true,
+        durable: true,
+        arguments: {'x-ha-policy' => 'all'}
+      }
+    end
+
+    def self.queue_name
+      [exchange_name, routing_key].join('#')
+    end
+
+    def self.exchange_params
+      {
+        durable: true,
+        auto_delete: false
+      }
+    end
+
+    def self.exchange_name
+      "test_exchange"
+    end
+
+    def self.routing_key
+      "rabbit_jobs_test"
+    end
+
+    def self.publish_params
+      {
+        persistent: true,
+        nowait: false
       }
     end
   end
--- lib/rabbit_jobs/publisher.rb
+++ lib/rabbit_jobs/publisher.rb
@@ -10,20 +10,28 @@ module RabbitJobs
       raise ArgumentError unless klass
 
       host = Configuration.host
-      queue_name = Configuration.queue[:name]
-      queue_params = Configuration.queue[:params]
+      queue_name = Configuration.queue_name
+      queue_params = Configuration.queue_params
+      exchange_name = Configuration.exchange_name
+      exchange_params = Configuration.exchange_params
+      publish_params = Configuration.publish_params
+      routing_key = Configuration.routing_key
 
       message_text = ([klass.to_s] + params).to_json
 
       AMQP.start(host: host) do |connection|
         channel  = AMQP::Channel.new(connection)
-        queue    = channel.queue(queue_name, queue_params)
-        exchange = channel.default_exchange
 
-        exchange.publish(message_text, :routing_key => queue.name) {
-          connection.close {
-            EM.stop
-          }
+        channel.on_error do |ch, channel_close|
+          puts "Channel-level error: #{channel_close.reply_text}, shutting down..."
+          connection.close { EM.stop }
+        end
+
+        exchange = channel.direct(exchange_name, exchange_params)
+        queue    = channel.queue(queue_name, queue_params).bind(exchange, :routing_key => routing_key)
+
+        exchange.publish(message_text, publish_params.merge({routing_key: routing_key})) {
+          connection.close { EM.stop }
         }
       end
     end
