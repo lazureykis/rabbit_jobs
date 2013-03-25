12611e01974908e88d897bf896fff776ac66d2e4
Pavel Lazureykis <lazureykis@gmail.com>
2013-03-25 11:58:33 +0400

    UPD ErrorMailer#send_letter

--- lib/rabbit_jobs/error_mailer.rb
+++ lib/rabbit_jobs/error_mailer.rb
@@ -1,35 +1,42 @@
 # -*- encoding : utf-8 -*-
 module RabbitJobs
   class ErrorMailer
-    def self.enabled?
-      !!(
-        defined?(ActionMailer) && \
-        RabbitJobs.config.mail_errors_from && !RabbitJobs.config.mail_errors_from.empty? && \
-        RabbitJobs.config.mail_errors_to && !RabbitJobs.config.mail_errors_to.empty?
-      )
-    end
+    class << self
+      def enabled?
+        return false unless defined?(ActionMailer)
+        config_present = RabbitJobs.config.mail_errors_from &&
+                         RabbitJobs.config.mail_errors_to &&
+                         RabbitJobs.config.mail_errors_from.size > 0 &&
+                         RabbitJobs.config.mail_errors_to.size > 0
+        !!config_present
+      end
 
-    def self.report_error(job, error = $!)
-      return unless enabled?
+      def send_letter(subject, body)
+        letter         = ActionMailer::Base.mail
+        letter.from    = RabbitJobs.config.mail_errors_from
+        letter.to      = RabbitJobs.config.mail_errors_to
+        letter.subject = subject
+        letter.body    = body
 
-      params ||= []
-
-      params_str = job.params == [] ? '' : job.params.map { |p| p.inspect }.join(', ')
-      subject = "RJ:Worker: #{error.class} on #{job.class}"
-      text    = "\n#{job.class}.perform(#{params_str})\n"
-      text   += "\n#{error.inspect}\n"
-      text   += "\nBacktrace:\n#{error.backtrace.join("\n")}" if error.backtrace
-
-      letter         = ActionMailer::Base.mail
-      letter.from    = RabbitJobs.config.mail_errors_from
-      letter.to      = RabbitJobs.config.mail_errors_to
-      letter.subject = subject
-      letter.body    = text
-
-      begin
         letter.deliver
-      rescue
-        RJ.logger.error [$!.message, $!.backtrace].flatten.join("\n")
+      end
+
+      def report_error(job, error = $!)
+        return unless enabled?
+
+        params = job.params || []
+
+        params_str = params.map { |p| p.inspect }.join(', ')
+        subject = "RJ:Worker: #{error.class} on #{job.class}"
+        text    = "\n#{job.class}.perform(#{params_str})\n"
+        text   += "\n#{error.inspect}\n"
+        text   += "\nBacktrace:\n#{error.backtrace.join("\n")}" if error.backtrace
+
+        begin
+          send_letter(subject, text)
+        rescue
+          RJ.logger.error [$!.message, $!.backtrace].flatten.join("\n")
+        end
       end
     end
   end
